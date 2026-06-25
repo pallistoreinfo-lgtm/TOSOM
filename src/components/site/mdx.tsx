9e9be6f0c2f4a8bbeeb88ebdda6532e90bee5a86
kamrulteam6-hash <kamrulteam6@gmail.com>
2026-06-25 20:49:13 +0600

    Update mdx.tsx

--- src/components/site/mdx.tsx
+++ src/components/site/mdx.tsx
@@ -17,7 +17,7 @@ const mdxComponents = {
         alt={props.alt ?? ""}
         width={900}
         height={600}
-        className="my-6 h-auto w-full rounded-md"
+        className="my-6 h-auto w-auto max-w-full max-h-64 rounded-md"
       />
     );
   },
